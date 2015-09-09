12018655499de7d70eeadcac3ca9f00ecaeeb921
andrew dang & abdul rahimi <student@epicodus.com>
2015-09-09 16:09:38 -0700

    checks if numbers are palindromes

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,21 @@
 var palindromeCheck = function(word) {
-  var split_word = word.split("");
-  var reversed_word = split_word.reverse();
 
-  if (word === reversed_word.join) {
+  if (isNaN(word) === false){
+    word = String(word);
+  }
+
+  // original words
+  var splitWord = word.split(" ");
+  var joinedWord = splitWord.join("");
+
+  // reversed words
+  var splitJoinedWord = joinedWord.split("");
+  var reversedWordArray = splitJoinedWord.reverse();
+  var reversedJoinedWord = reversedWordArray.join("");
+
+
+
+  if (joinedWord === reversedJoinedWord) {
     return true;
   } else {
     return false;
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,13 @@
 describe("palindromeCheck", function() {
-  it("checks to see if a word is a palindrome", function() {
-    expect(palindromeCheck("true")).to.equal(false);
+  // it("checks to see if a word is a palindrome", function() {
+  //   expect(palindromeCheck("mom")).to.equal(true);
+  // });
+  //
+  // it("checks to see if a phrase is a palindrome", function(){
+  //   expect(palindromeCheck("lion oil")).to.equal(true);
+  // });
+
+  it("checks to see if a number is a palindrome", function(){
+    expect(palindromeCheck(10001)).to.equal(true)
   });
 });

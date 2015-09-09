fe4d2f95556a6f2f3e87c4098bc146985f0812ed
andrew dang & abdul rahimi <student@epicodus.com>
2015-09-09 16:45:36 -0700

    add front end

--- js/scripts.js
+++ js/scripts.js
@@ -5,20 +5,32 @@ var palindromeCheck = function(word) {
   }
 
   // original words
-  var splitWord = word.split(" ");
+  var splitWord = word.toLowerCase().split(" ");
   var joinedWord = splitWord.join("");
 
   // reversed words
-  var splitJoinedWord = joinedWord.split("");
+  var splitJoinedWord = joinedWord.toLowerCase().split("");
   var reversedWordArray = splitJoinedWord.reverse();
   var reversedJoinedWord = reversedWordArray.join("");
 
 
 
   if (joinedWord === reversedJoinedWord) {
-    return true;
+    return "This is a palindrome!";
   } else {
-    return false;
+    return "This is not a palindrome!";
   }
 
 };
+
+$(document).ready(function() {
+  $("form#phrase").submit(function(event){
+    var string = $("input#phrase").val()
+    var result = palindromeCheck(string)
+
+    $(".palindrome-result").text(result);
+
+    $("#result").show();
+    event.preventDefault();
+  })
+})
